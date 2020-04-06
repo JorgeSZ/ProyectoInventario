d3efe3b5b66b7fd3447a94bba763fb97dd17e8fe
denniscr90 <60120573+denniscr90@users.noreply.github.com>
2020-04-05 23:43:39 -0600

    Se agrega tabla Productos

---
+++ Proyecto/Tabla Productos y tipos productos.sql
@@ -0,0 +1,90 @@
+create database Proyecto_Inventario
+
+
+use Proyecto_Inventario
+
+--Tabla Producto , no incluye precio ya que se calcula , pendiente
+
+create table Producto (
+	idProducto int not null primary key ,
+	desProducto varchar (50) not null , 
+	codTipoProducto int not null , 
+	costo decimal (9,2) not null ,
+	porcUtilidad decimal (9,2) not null ,
+	codProveedor int not null , 
+	--Calcularprecio decimal (9,2) null
+	)
+
+	select * from Producto
+	
+---------------------------------------------------------
+-- Procedimiento que inserta Productos
+---------------------------------------------------------
+
+CREATE PROCEDURE [dbo].[Insertar_Producto] 
+	    @Id int ,  
+		@descripcionprod Varchar(50) , 
+		@Tipo int , 
+		@Cost decimal (10,2), 
+		@Utilidad decimal (10,2),
+		@proveedor int 
+
+
+AS
+BEGIN
+	
+	INSERT INTO Producto (idProducto, desProducto, codTipoProducto, costo,porcUtilidad,codProveedor)
+
+		VALUES(
+		@Id,  
+		@descripcionprod  , 
+		@Tipo , 
+		@Cost , 
+		@Utilidad ,
+		@proveedor
+		)
+
+END
+
+GO
+
+---------------------------------------------------------
+-- creacion de tabla Tipo Producto
+---------------------------------------------------------
+Create table Tipo_Producto(
+
+	Id int not null IDENTITY(1,1) primary key ,
+	TipoProducto varchar (50) not null , 
+	
+	)
+
+	Create table Tipo_Producto(
+
+	Id int not null  primary key ,
+	TipoProducto varchar (50) not null , 
+	
+	)
+	
+	--------------------------------------------------
+	--Select
+	--------------------------------------------------
+
+	select * from Tipo_Producto	
+
+	---------------------------------------------------
+	------- Insert para ingresar tipos de productos para pruebas , se debe crear procedimiento para insertar valores
+	-------en caso que se requiera cambiar el tipo de producto, necesita ventana de mantenimiento
+	---------------------------------------------------
+
+	INSERT INTO Tipo_Producto ( TipoProducto)
+	values
+	('Frutas'),
+	('Verduras'),
+	('Legumbres'),
+	('Leguminosas')
+	
+
+	select * from Tipo_Producto
+
+
+
