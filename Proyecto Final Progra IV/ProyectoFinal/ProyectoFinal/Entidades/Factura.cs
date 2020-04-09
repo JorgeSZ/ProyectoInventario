f004a8d62751d8e9788409005f98927e5e443fd9
Jainier <64080492+Jainier@users.noreply.github.com>
2020-04-08 20:46:25 -0600

    FrmFactura Inconcluso
    Creación del form y métodos a utilizar

---
+++ Proyecto Final Progra IV/ProyectoFinal/ProyectoFinal/Entidades/Factura.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal.Entidades
+{
+   public class Factura
+    {
+        int numFactura { get; set; }
+        int idProveedor { get; set; }
+        string fechaFactura { get; set; }
+        int tipoPago { get; set; } 
+        string fechaVencimiento { get; set; }
+         double subTotal { get; set; }
+
+        double IVA { get; set; }
+        double descuento { get; set; }
+        double totalFactura { get; set; }
+
+        
+        public double funcTotal(double sub, double iva, double desc)
+        {
+            return sub + iva + desc;
+        }
+
+    }
+
+    
+}
